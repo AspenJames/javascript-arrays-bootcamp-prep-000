35d27681768d2b381a1693015057ab5637a1e78e
Aspen James <aspenjames@tqca.org>
2018-01-14 21:33:11 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,17 @@
+chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+function addElementToBeginningOfArray(array, element){
+  newArray = [element, ...array];
+  return newArray
+}
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array = array.unshift(element)
+  return array
+}
+function addElementToEndOfArray(array, element){
+  newArray = [...array, element];
+  return newArray
+}
+function destructivelyAddElementToEndOfArray(array, element){
+  array = array.push(element)
+  return array
+}
